a1889de3f9d3babfd8a01c2b678487fc8b780675
Matheus Soares <matheus.soares8890@gmail.com>
2024-04-04 18:00:53 -0300

    updata: main

--- src/main.py
+++ src/main.py
@@ -3,18 +3,11 @@ from scripts import preprocessingdatabase
 from scripts import plot as plot
 import pandas as pd
 
-# Define os parâmetros DH para cada elo
-Elos = [['theta_1',10,0,90,0],
-        ['theta_2',0,18,180,0],
-        ['theta_3',0,18,-180,0]
-        ]
-
-# Cria o robo por meio dos parâmetros
-path = f'C:/Users/je7560/Downloads/dataset.csv'
+path = f'src/data/raw/dataset.csv'
 dataset = pd.read_csv(path)
 
 
-plot.plot3D(dataset,['p_x','p_y','p_z'])
+#plot.plot3D(dataset,['p_x','p_y','p_z'])
 
 
 #print(robo.nameBot)
