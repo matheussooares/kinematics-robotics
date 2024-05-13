6cc007e232afe367fa969c4f4a4fad51736a999b
Matheus Soares <50305335+matheussooares@users.noreply.github.com>
2024-05-13 13:13:45 -0300

    update: model mlp and machinelearning

--- src/kinematicsrobotics/modelmlp.py
+++ src/kinematicsrobotics/modelmlp.py
@@ -1,5 +1,6 @@
 from sklearn.metrics import mean_squared_error
 from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
 
 def predict_mse(model,X_test,Y_test):
     y_predic = model.predict(X_test)
@@ -9,3 +10,6 @@ def modelo(hidden):
     func_act = 'tanh'
     model = MLPRegressor(hidden_layer_sizes=hidden,
                         activation=func_act)
+
+def split(x, y, test_size):
+    return train_test_split(x, y, test_size=test_size)
