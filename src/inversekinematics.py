eae260e196d9983cc392b93c1acbbe15454bdbec
Matheus Soares <50305335+matheussooares@users.noreply.github.com>
2024-08-05 13:42:19 -0300

    update crossvalidation

--- src/inversekinematics.py
+++ src/inversekinematics.py
@@ -1,46 +1,49 @@
 from kinematicsrobotics.datahandler import Extract
 from kinematicsrobotics.dataprocessing import Preprocessing
 from kinematicsrobotics.model import Model
+from kinematicsrobotics.crossvalidation import ParameterSearchMLP
+
+# Base de dados
+ext = Extract()
+
+dataset = ext.dataframe(r'src\data\ready\dataset-radius-1cm.csv')
+
+# Divisão dos dados
+size_train,size_val,size_test = 0.7, 0.2, 0.1
+
+data  = Preprocessing(dataset = dataset, 
+                           x_labels=['p_x', 'p_y','p_z', 'roll', 'pich', 'yaw'],
+                           y_labels=['theta_1', 'theta_2', 'theta_3', 'theta_4']
+                        )
+
+x_train, x_test, y_train, y_test = data.data_train_test
 
 
-# Extração das bases de dados
-extrator = Extract()
+# Modelo
+mlp = Model.mlp_regressor()
 
-dataset = extrator.dataframe(path_data = r'src\data\ready\dataset-radius-0.5cm.csv')
+# Espaço de busca do grid search
+min_neurons= [50, 100, 100, 100]
+max_neurons = [3000, 350, 300, 300]
+step = [5, 10, 25, 50]
+layers = [1, 2, 3, 4]
+activation =  ['relu', 'tanh']
 
-# Pré-processamento da base de dados
-processed_data = Preprocessing(dataset = dataset, 
-                               x_labels=['p_x', 'p_y','p_z', 'roll', 'pich', 'yaw'],
-                               y_labels=['theta_1', 'theta_2', 'theta_3', 'theta_4']
-                               )
 
-x_train, x_test, y_train, y_test = processed_data.data_train_test
+cv = ParameterSearchMLP(min_neurons = min_neurons, 
+                        max_neurons = max_neurons, 
+                        num_layers = layers, 
+                        step = step,
+                        model = mlp, 
+                        x_train = x_train,
+                        y_train = y_train,
+                        n_splits = 2
+)
 
-# Estimação de hiperparâmetros
-history = extrator.dataframe(r'src\data\ready\history.csv')
+cv.parameter(activation = activation)
 
-history_best = history[history['rank_test_score'] == 1]
-
-params = eval(history_best.iloc[0]['params'])
-
-#  Modelos de predição
-mlp = Model.mlp_regressor(**params)
-
-# Treinamento
-mlp.fit(x = x_train, y = y_train)
-
-y_est = mlp.predict(x = x_train)
-
-print(y_est)
-
-# print(f"Melhor erro: {model.best_loss_}\n")
-
-# # plt.plot(model.loss_curve_)
-# # plt.title('Erro Quadratico')
-# # plt.xlabel('Épocas')
-# # plt.ylabel('MSE')
-# # plt.show()
-
-# ## Métrica
-# mse = modelmlp.predict_mse(model,X_test,Y_test)
-# print(mse)
+best_estimator = cv.RandomizedSearch(n_iter = 1, 
+                                     path_cv_results = r'src\data\history\parametersearch-MLP\cv_results.csv', 
+                                     path_best_params = r'src\data\history\parametersearch-MLP\best_params.csv'
+                                     
+)
--- src/kinematicsrobotics/crossvalidation.py
+++ src/kinematicsrobotics/crossvalidation.py
@@ -1,83 +1,101 @@
-from sklearn.model_selection import ShuffleSplit,RandomizedSearchCV,GridSearchCV
-from itertools import product
-from sklearn.metrics import mean_squared_error,r2_score
+from kinematicsrobotics.datahandler import Save
+from kinematicsrobotics.model import Model
 from pandas import DataFrame
+from itertools import product
+from sklearn.model_selection import ShuffleSplit, RandomizedSearchCV, GridSearchCV
 
-# Calcula a porcentagem relativa dos dados de treino e validação
-def size_split(size_train,size_val):
-    size_val = (1 - size_train/(size_train+size_val))
-    return size_val
-
-# Espaço de busca dos neurônios
-def space_hidden(min_neurons,max_neurons,layers,step):
-    # Lista que armazena os neurônios
-    neuron_space = []
+class ParameterOptimizer:
     
-    # Gera a lista de possíveis números de neurônios em cada camada
-    possible_neurons = list(range(min_neurons, max_neurons + 1,step))
+    def __init__(self, *, model: Model, x_train, y_train, size_train = 0.7, size_val =  0.1, size_test = 0.2, n_splits: int = 4) -> None:
+        self._save = Save()
+        self._model = model
+        self._x = x_train
+        self._y = y_train
+        self.size_validation(size_train = size_train, 
+                             size_val = size_val,
+                             size_test = size_test
+        )
+        self.holdout(n_splits = n_splits)
     
-    # Crie todas as combinações possíveis
-    for num_layers in layers:
-        neuron_combinations = list(product(possible_neurons, repeat=num_layers))
-        neuron_space.extend(neuron_combinations)
+    # Método público que define a divisão dos dados de treino e validação
+    def size_validation(self, *, size_train, size_val, size_test):
+        self._size_val = (1 - size_train/(size_train+size_val))
+        self._size_train = 1 - self._size_val
+        self._size_test = size_test
 
-    return neuron_space
-
-# Parâmetro da rede mlp
-def parameter_mlp(min_neurons,max_neurons,layers,step,func_act): 
-    # Dicionário que armazena os hiperparâmetros
-    param_grid = {
-        'hidden_layer_sizes': space_hidden(min_neurons, max_neurons,layers,step),
-        'activation': func_act
-    }
-
-    return param_grid
-
-# Random Search
-def RandomizedSearch(x, y, model, param_grid, scoring,cv, n_iter):
-    # Configura os parâmetros da técnica de otimização 
-    random_search = RandomizedSearchCV(estimator=model, 
-                                       param_distributions=param_grid, 
-                                       scoring=scoring, 
-                                       cv=cv, 
-                                       n_iter=n_iter, 
-                                       random_state=42, 
-                                       return_train_score=True,
-                                       verbose=0)
+    # Validação cruzada hold out
+    def holdout(self, *, n_splits = 4):
+        self._n_splits = n_splits
+        
+        self._cv = ShuffleSplit(n_splits=n_splits, 
+                                test_size = self._size_val, 
+                                random_state=42
+        )
     
-    # Treina os modelos
-    random_search.fit(x, y)
+       
+class ParameterSearchMLP(ParameterOptimizer):
+    def __init__(self, *, min_neurons: int, max_neurons: int, num_layers: int, step: int, **kw) -> None:
+        self._min_neurons = min_neurons
+        self._max_neurons = max_neurons
+        self._num_layers = num_layers
+        self._step = step
+        super().__init__(**kw)
 
-    # DataFrame que armazena os resultado dos hiperparâmetros
-    history = DataFrame(random_search.cv_results_)
+    def RandomizedSearch(self,*, scoring = 'neg_mean_squared_error', n_iter, path_cv_results = None, path_best_params = None):
+        # Configura os parâmetros da técnica de otimização 
+        random_search = RandomizedSearchCV(estimator = self._model.model, 
+                                           param_distributions = self.param_grid, 
+                                           scoring = scoring, 
+                                           cv = self._cv, 
+                                           n_iter = n_iter, 
+                                           random_state = 42, 
+                                           return_train_score = True,
+                                           verbose = True
+        )
+        
+        # Treina os modelos
+        random_search.fit(self._x, self._y)
 
-    # Melhor hiperparâmetro
-    best_estimator = random_search.best_estimator_
+        # DataFrame que armazena os resultado dos hiperparâmetros
+        history = DataFrame(random_search.cv_results_)
 
-    return history,best_estimator
+        best_params = DataFrame(random_search.best_params_)
 
-# Grid Search
-def GridSearch(x, y, model, param_grid, scoring, cv):
-    # Configura os parâmetros da técnica de otimização 
-    gridshearch = GridSearchCV(estimator=model,
-                               param_grid=param_grid,
-                               cv=cv,
-                               scoring=scoring,
-                               verbose=0,
-                               return_train_score=True)
+        if path_cv_results:
+            self._save.dataframe(data = history, path_data = path_cv_results)
+        
+        if path_best_params:
+            self._save.dataframe(data = best_params, path_data = path_best_params)
+
+        # Melhor hiperparâmetro
+        
+       
+
+        return random_search.best_estimator_
     
-    # Treina os modelos
-    gridshearch.fit(x, y)
-    
-    # Armazena os resultado dos hiperparâmetros
-    history = DataFrame(gridshearch.cv_results_)
-    
-    best_estimator = gridshearch.best_estimator_
+    def parameter(self, *, activation):
+        param_grid = []
+        
+        for i, layers in enumerate(self._num_layers):
+            hidden_layer = self.space_hidden(layers = layers, 
+                                             min_neurons = self._min_neurons[i], 
+                                             max_neurons = self._max_neurons[i], 
+                                             step = self._step[i]
+            )
 
-    return history,best_estimator
+            param = { 
+                'hidden_layer_sizes': hidden_layer,
+                'activation': activation
+            }
+            param_grid.append(param)
+ 
+        self.param_grid = param_grid
+            
 
-# Validação cruzada hold out
-def holdout(n_splits,test_size):
-    return ShuffleSplit(n_splits=n_splits, 
-                      test_size=test_size, 
-                      random_state=42)
+    def space_hidden(self,*, layers, min_neurons, max_neurons, step):    
+        # Gera a lista de possíveis números de neurônios em cada camada
+        possible_neurons = list(range(min_neurons, max_neurons + 1, step))
+
+        return list(product(possible_neurons, repeat=layers))
+
+    
