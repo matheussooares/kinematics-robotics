6e04fbcd59a156488ec4ff8c9769aa4650f91747
Matheus Soares <matheus.soares8890@gmail.com>
2024-09-18 23:45:51 -0300

    add: forward toolboox

---
+++ src/forwardkinematicstoolbox.py
@@ -0,0 +1,21 @@
+import roboticstoolbox as rtb
+import matplotlib.pyplot as plt
+from math import pi  # Importa o valor de pi
+
+# ---------Parâmetros DH----------
+# theta, d, a, alpha
+L1 = rtb.RevoluteDH(d=0.1, a=0, alpha=pi/2, offset = 0)   # Criando elo 0
+L2 = rtb.RevoluteDH(d=0, a=0.18, alpha=pi, offset = 0)     # Criando elo 1
+L3 = rtb.RevoluteDH(d=0, a=0.18, alpha=-pi, offset = 0)     # Criando elo 2
+L4 = rtb.RevoluteDH(d=0, a=0, alpha=pi/2, offset = pi/2)     # Criando elo 3
+L5 = rtb.RevoluteDH(d=0.18, a=0, alpha=0, offset = 0)     # Criando elo 4
+
+# ---------Criação do robô----------
+robo = rtb.DHRobot([L1, L2, L3, L4, L5], name="Robô Didático")
+
+# Ângulos predefinidos
+qf0 = [[0, 0, 0, 0, 0], [0, pi/3, pi/2, -pi/4, 0]]
+
+
+# Interface gráfica teach do robô
+robo.teach(qf0[1])  # Passa os ângulos predefinidos como argumento
